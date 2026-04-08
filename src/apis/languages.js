47b290edad476e937ab48eb6b389a061116b235a
Prajwal Chavan <prajwalchavan@Prajwals-MacBook-Air.local>
2026-04-08 14:52:15 +0530

    dropdown added and dashboard updated

---
+++ src/apis/languages.js
@@ -0,0 +1,13 @@
+import api from  "./api";
+
+export const Languages = async()=>{
+    try{
+        const response = await api.get('/auth/languages/');
+        console.log(response);
+        return response.data;
+
+    }catch(error){
+        console.error("fetch languages error:",error);
+        throw error;
+    }
+}
--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import { runCode } from '../../apis/runCode';
+import { Languages } from '../../apis/languages';
+import Select from "react-select";
+
 export default function Dashboard(){
     const [code,setCode] = React.useState("");
-    const [input,setInput] = React.useState("");
+    // const [input,setInput] = React.useState("");
     const [output,setOutput] = React.useState("");
-    const [language, setLanguage] = React.useState("python");
+    const [languages, setLanguages] = React.useState([]); // list
+    const [language, setLanguage] = React.useState("");   // selected
+    const options = languages.map(lang=>({
+        value:lang,
+        label:lang
+    }))
+    
+    React.useEffect(()=>{
+        const fetchLanguages = async ()=>{
+           const data = await Languages();
+           console.log("Supported languages:",data);
+           setLanguages(data.languages);
+           setLanguage(data.languages[0]);
+           return data;
+        }
+        fetchLanguages();
+    },[]);
 
     const handleRun = async () => {
         try {
-            const result = await runCode(code, input, language);
+            const result = await runCode(code, language);
             console.log("Code execution result:", result);
             setOutput(result);
         } catch (error) {
@@ -23,13 +42,33 @@ export default function Dashboard(){
                 <h2>Online Code Runner</h2>
 
                 <div className="controls">
-                <select
-                    value={language}
-                    onChange={(e) => setLanguage(e.target.value)}
-                >
-                    <option value="python">Python</option>
-                    <option value="javascript">JavaScript</option>
-                </select>
+                  {/* <select value={language} onChange={(e)=>setLanguage(e.target.value)}>
+                     {languages.map((lang,idx)=>(
+                        <option key={idx} value={lang}>
+                            {lang}
+                        </option>
+                     ))}
+                  </select> */}
+                  <Select
+                    options={options}
+                    onChange={(opt) => setLanguage(opt.value)}
+                    styles={{
+                        control: (base) => ({
+                        ...base,
+                        backgroundColor: "#1a1c23",
+                        color: "white",
+                        }),
+                        menu: (base) => ({
+                        ...base,
+                        backgroundColor: "#1a1c23",
+                        }),
+                        option: (base, state) => ({
+                        ...base,
+                        backgroundColor: state.isFocused ? "#333" : "#1a1c23",
+                        color: "white",
+                        }),
+                    }}
+                    />
 
                 <button onClick={handleRun}>▶ Run</button>
                 </div>
@@ -46,12 +85,12 @@ export default function Dashboard(){
 
                 <div className="right-panel">
 
-                <textarea
+                {/* <textarea
                     className="input-box"
                     placeholder="// enter input here"
                     value={input}
                     onChange={(e) => setInput(e.target.value)}
-                />
+                /> */}
 
                 <textarea
                     className="output-box"
