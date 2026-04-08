a38c5c1dcc82fe316dccd4509ca9fa4bc5cd47b8
Prajwal Chavan <prajwalchavan@Prajwals-MacBook-Air.local>
2026-04-08 17:18:54 +0530

    log commented

--- src/apis/languages.js
+++ src/apis/languages.js
@@ -3,7 +3,7 @@ import api from  "./api";
 export const Languages = async()=>{
     try{
         const response = await api.get('/auth/languages/');
-        console.log(response);
+        // console.log(response);
         return response.data;
 
     }catch(error){
